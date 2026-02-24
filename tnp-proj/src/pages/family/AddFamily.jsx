67268f33c82bd606fb6ad4f26c865673af17b446
kukz <futuretechie4u@gmail.com>
2026-02-24 22:37:28 +0530

    Rename CSS import for AddFamily component

--- tnp-proj/src/pages/family/AddFamily.jsx
+++ tnp-proj/src/pages/family/AddFamily.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "../../css/AddFamily.css";
+import "../../css/addfamily.css";
 
 const AddFamily = () => {
 
